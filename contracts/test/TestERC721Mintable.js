01c0d7082ceed45e4e13179343c1b3a825915a56
Daniel Abrao <danielpaladar@gmail.com>
2019-04-03 22:12:50 -0300

    Update test

--- contracts/test/TestERC721Mintable.js
+++ contracts/test/TestERC721Mintable.js
@@ -3,16 +3,6 @@ var ERC721MintableComplete = artifacts.require('SolnSquareVerifier');
 //var ERC721MintableComplete = artifacts.require('ERC721MintableComplete');
 //var ERC721MintableComplete = artifacts.require('CustomERC721Token');
 let proof = require("../../zokrates/code/square/proof");
-let proof1 = require("../../zokrates/code/square/proof_1");
-let proof2 = require("../../zokrates/code/square/proof_2");
-let proof3 = require("../../zokrates/code/square/proof_3");
-let proof4 = require("../../zokrates/code/square/proof_4");
-let proof5 = require("../../zokrates/code/square/proof_5");
-let proof6 = require("../../zokrates/code/square/proof_6");
-let proof7 = require("../../zokrates/code/square/proof_7");
-let proof8 = require("../../zokrates/code/square/proof_8");
-let proof9 = require("../../zokrates/code/square/proof_9");
-let proof10 = require("../../zokrates/code/square/proof_10");
 
 contract('TestERC721Mintable', accounts => {
 
@@ -37,50 +27,7 @@ contract('TestERC721Mintable', accounts => {
                     proof.proof.K, 
                     proof.input,
                     {from: account_one});
-                    await this.contract.mintVerified(account_two, 2, 
-                        proof4.proof.A, 
-                        proof4.proof.A_p, 
-                        proof4.proof.B, 
-                        proof4.proof.B_p, 
-                        proof4.proof.C, 
-                        proof4.proof.C_p, 
-                        proof4.proof.H, 
-                        proof4.proof.K, 
-                        proof4.input,
-                        {from: account_one});
-                    await this.contract.mintVerified(account_two, 3, 
-                        proof1.proof.A, 
-                        proof1.proof.A_p, 
-                        proof1.proof.B, 
-                        proof1.proof.B_p, 
-                        proof1.proof.C, 
-                        proof1.proof.C_p, 
-                        proof1.proof.H, 
-                        proof1.proof.K, 
-                        proof1.input,
-                        {from: account_one});
-                    await this.contract.mintVerified(account_two, 4, 
-                        proof2.proof.A, 
-                        proof2.proof.A_p, 
-                        proof2.proof.B, 
-                        proof2.proof.B_p, 
-                        proof2.proof.C, 
-                        proof2.proof.C_p, 
-                        proof2.proof.H, 
-                        proof2.proof.K, 
-                        proof2.input,
-                        {from: account_one});
-                    /* await this.contract.mintVerified(account_two, 5, 
-                        proof3.proof.A, 
-                        proof3.proof.A_p, 
-                        proof3.proof.B, 
-                        proof3.proof.B_p, 
-                        proof3.proof.C, 
-                        proof3.proof.C_p, 
-                        proof3.proof.H, 
-                        proof3.proof.K, 
-                        proof3.input,
-                        {from: account_one}); */
+
             
         })
 
